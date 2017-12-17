501db2014596231df57f8a725b1873db3ff65f1c
konstin <konstin@mailbox.org>
2017-12-17 15:26:13 +0100

    Add simple stats script
    It shows simply oparl usage and was quite useful to me

---
+++ stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import sys
+
+import requests
+
+with open("endpoints.list") as fp:
+    lines = fp.readlines()
+
+urls = []
+for line in lines:
+    line = "/".join(line.strip().split("/")[:-1])
+    if not line.startswith(";") and "sdnetrim" in line:
+        urls.append(line)
+
+for url in urls:
+    print(url)
+    name = requests.get(url + "/body/1").json()["name"]
+    papers = requests.get(url + "/body/1/paper").json()["pagination"]["totalElements"]
+    meeting = requests.get(url + "/body/1/meeting").json()["pagination"]["totalElements"]
+    person = requests.get(url + "/body/1/person").json()["pagination"]["totalElements"]
+    organization = requests.get(url + "/organization").json()["pagination"]["totalElements"]
+
+    print("{} has {} papers, {} meetings, {} persons, {} organizations".format(name, papers, meeting, person,
+                                                                               organization))
+
+if __name__ == '__main__':
+    pass
